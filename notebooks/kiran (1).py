f65fad1300845035dd05b8f7d035c360c887e301
Kiran2kinghotmailcom <45938307+Kiran2kinghotmailcom@users.noreply.github.com>
2020-10-01 01:14:56 +0530

    Update: README.md notebooks

---
+++ notebooks/kiran (1).py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1 + 1
+
+
+# In[2]:
+
+
+print('Hello World!')
+
+
+# In[3]:
+
+
+def say_hello(recipient):
+    return 'Hello, {}!'.format(recipient)
+
+say_hello('Tim')
+
+
+# In[ ]:
+
+
+
+
+# COMMAND ----------
+
+# MAGIC %sh curl -n https://cust-success.cloud.databricks.com/#setting/clusters/api/1.2/clusters/list
+
+# COMMAND ----------
+
+# MAGIC %sh curl -n https://cust-success.cloud.databricks.com/#setting/clusters/api/1.2/clusters/list | running
+
+# COMMAND ----------
+
+
+print('abc')
+df = spark.sql("select 1, 2, 35, 1, 2, 3, 4,1, 2, 35, 1, 2, 3, 4,1, 2, 35, 1, 2, 3, 4,1, 2, 35, 1, 2, 3, 4,1, 2, 35, 1, 2, 3, 4,5 as this_is_a_very_long_name_this_is_a_very_long_name5_this_is_a_very_long_name_this_is_a_very_long_name56")
+display(df)
